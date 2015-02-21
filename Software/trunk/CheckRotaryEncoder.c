5406a27a2b46e7a91cb52d17216b6a69fa31670c
kubi48 <kubi48@6ebdd44f-5a7e-449c-b779-36259138d2c7>
2015-02-21 20:02:12 +0000

    corrected 8x16 font,  display refresh for ST7920 rotary e. check
    git-svn-id: svn://mikrocontroller.net/transistortester@396 6ebdd44f-5a7e-449c-b779-36259138d2c7

--- Software/trunk/CheckRotaryEncoder.c
+++ Software/trunk/CheckRotaryEncoder.c
@@ -28,6 +28,9 @@ void CheckRotaryEncoder(void) {
   message_key_released(RotaryEncoder_str);	// RotaryEncoder in line 1
   lcd_line2();
   lcd_MEM_string(TURN_str);			// "turn!" at line 2 of LCD
+#if (LCD_ST_TYPE == 7920)
+  lcd_refresh();
+#endif
   
   for (ii=0;ii<4;ii++) {
      TP13tab[ii] = 0;		//reset count of bit TP1:TP3 to zero
